35bec61cce8f8cb003235cd95ed0925187ebf6ed
hakata <isu.hakata@gmail.com>
2022-07-23 08:39:02 +0000

    fix

--- sql/init.sql
+++ sql/init.sql
@@ -2,6 +2,6 @@ DELETE FROM tenant WHERE id > 100;
 DELETE FROM visit_history WHERE created_at >= '1654041600';
 UPDATE id_generator SET id=2678400000 WHERE stub='a';
 ALTER TABLE id_generator AUTO_INCREMENT=2678400000;
-ALTER TABLE visit_history ADD INDEX `idx_tenant_competition` (`tenant_id, competitioin_id`),
-ALTER TABLE visit_history ADD INDEX `idx_tenant_id_player_id` (`tenant_id`, `player_id`),
-ALTER TABLE visit_history ADD INDEX `idx_competition_id_player_id` (`competition_id`, `player_id`)
+CREATE INDEX  `idx_tenant_competition` ON visit_history (`tenant_id`, `competition_id`);
+CREATE INDEX  `idx_tenant_id_player_id` ON visit_history (`tenant_id`, `player_id`);
+-- CREATE INDEX  `idx_competition_id_player_id` ON visit_history (`competition_id`, `player_id`);
