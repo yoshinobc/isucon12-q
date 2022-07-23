1caae55608f94556e3f50dd9d6b71e351f534e02
hiroto7 <32275337+hiroto7@users.noreply.github.com>
2022-07-23 11:49:26 +0900

    LIMIT 1

--- go/isuports.go
+++ go/isuports.go
@@ -19,6 +19,7 @@ import (
 	"time"
 
 	_ "net/http/pprof"
+
 	"github.com/go-sql-driver/mysql"
 	"github.com/gofrs/flock"
 	"github.com/jmoiron/sqlx"
@@ -395,7 +396,7 @@ type PlayerRow struct {
 // 参加者を取得する
 func retrievePlayer(ctx context.Context, tenantDB dbOrTx, id string) (*PlayerRow, error) {
 	var p PlayerRow
-	if err := tenantDB.GetContext(ctx, &p, "SELECT * FROM player WHERE id = ?", id); err != nil {
+	if err := tenantDB.GetContext(ctx, &p, "SELECT * FROM player WHERE id = ? LIMIT 1", id); err != nil {
 		return nil, fmt.Errorf("error Select player: id=%s, %w", id, err)
 	}
 	return &p, nil
